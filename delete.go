9e728ad4780f7c1e3762a93cc78d20e47b9c958f
Oliviero Taleghani <o.taleghani@talesign.com>
2024-05-20 11:24:10 +0200

    Delete operation

---
+++ delete.go
@@ -0,0 +1,17 @@
+package orm
+
+import (
+	"fmt"
+	"reflect"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Delete(model interface{}, condition string, args ...interface{}) error {
+    t := reflect.TypeOf(model)
+    tableName := t.Name()
+    query := fmt.Sprintf("DELETE FROM %s WHERE %s;", tableName, condition)
+    _, err := db.Exec(query, args...)
+    return err
+}
+
